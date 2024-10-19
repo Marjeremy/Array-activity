5c04d4949b566a6226e7caf201079ab19d154ae7
Marjeremy <gaoat856@gmail.com>
2024-10-19 20:43:10 +0800

    Array-activity commit

---
+++ arrayActivity.js
@@ -0,0 +1,10 @@
+const favSinger = ["jeremy zucker", "Post Malone", "Ed sheeran"];
+console.log(favSinger[2]);
+
+const favNumber = [24, 28, 24, 6];
+
+const mixedArr = ["String", ["otherarray"], 123, true ];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
